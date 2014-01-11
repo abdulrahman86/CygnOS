165080c74f698645645f903aac2d571cda795efc
Sunny Shah <shahsunny712@gmail.com>
2014-01-11 23:39:26 +0530

    Loading of root directory with required(first few) functions added. (Error in reading sectors in this commit)

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -8,24 +8,24 @@ bpbOEMName		DB "Sunny S "
 ;#################### BIOS Parameter Block 
 ;##########################################
 
-bpbBytesPerSector		DW 512
-bpbSectorsPerCluster	DB 1
-bpbReservedSectors		DW 1
-bpbNumberOfFATs			DB 1
-bpbNumberOfRootEnt		DW 224
-bpbTotalSectors			DW 2880
-bpbMedia				DB 0xF0
-bpbSectorsPerFAT		DW 9
-bpbSectorsPerTrack		DW 18
-bpbNumberOfHeads		DW 2
-bpbHiddenSectors		DD 0
-bpbBigSectors			DD 0
-ebpbDriveNumber			DB 0x0
-ebpbFlags				DB 0
-ebpbSignature			DB 0x29
-ebpbVolumeID			DD 0xAAABACAD
-ebpbVolume				DB "BOOT FLOPPY"
-ebpbFileSystem			DB "FAT12   "
+bpbBytesPerSector	 :	DW 512
+bpbSectorsPerCluster :	DB 1
+bpbReservedSectors	 :	DW 1
+bpbNumberOfFATs		 :	DB 2
+bpbNumberOfRootEnt	 :	DW 224
+bpbTotalSectors		 :	DW 2880
+bpbMedia			 :	DB 0xF0
+bpbSectorsPerFAT	 :	DW 9
+bpbSectorsPerTrack	 :	DW 18
+bpbNumberOfHeads	 :	DW 2
+bpbHiddenSectors	 :	DD 0
+bpbBigSectors		 :	DD 0
+ebpbDriveNumber		 :	DB 0x0
+ebpbFlags			 :	DB 0
+ebpbSignature		 :	DB 0x29
+ebpbVolumeID		 :	DD 0xAAABACAD
+ebpbVolume			 : 	DB "BOOT FLOPPY"
+ebpbFileSystem		 :	DB "FAT12   "
 
 ;#########################################
 ;############ Bootloader code begins here
@@ -48,7 +48,9 @@ __SEGMENTS_INIT:
 	jmp __LOAD_ROOT
 	
 __VARIABLES_DECL:
-	msgBootStarting DB "Loading MyOS...", 0x0D, 0x0A, 0x00
+	msgBootStarting  DB "Loading MyOS...", 0x0D, 0x0A, 0x00
+	msgRootDirLoaded DB "Loaded root directory...", 0x0D, 0x0A, 0x00
+	msgFailure DB "Error in loading the operating system...", 0x0D, 0x0A, 0x00
 	
 __FUNCTIONS_DECL:
 	_PrintMessage:
@@ -60,19 +62,79 @@ __FUNCTIONS_DECL:
 		jmp _PrintMessage
 	Done:
 		ret
+	
+	_GetSectors:
+		push ax
+		push bx
+		push cx
+		
+		call _LBAtoCHS
+		mov ah, 0x02
+		mov al, 0x01
+		mov dl, BYTE [ebpbDriveNumber]
+		;push ax
+		;mov ah, 0Eh
+		;mov al, cl
+		;int 10h
+		;pop ax
+		int 13h
+		jnc ReadSuccess
+		mov si, msgFailure
+		call _PrintMessage
+		;mov al, ah
+		;mov ah, 0Eh
+		;int 10h
+		jmp __END_AND_FAILURE
+		
+	ReadSuccess:
+		pop cx
+		pop bx
+		pop ax
+		inc ax
+		add bx, WORD [bpbBytesPerSector]
+		loop _GetSectors
+		;;;;;mov si, msgRootDirLoaded
+		;;;;;call _PrintMessage
+		ret
+	
+	_LBAtoCHS:
+		xor dx, dx
+		div WORD [bpbSectorsPerTrack]
+		inc dl
+		mov cl, dl
+		xor dx, dx
+		div WORD [bpbNumberOfHeads]
+		mov dh, dl
+		mov ch, al
+		ret
 
 __LOAD_ROOT:
 	mov si, msgBootStarting
 	call _PrintMessage
 	
+	xor ax, ax
+	mov cx, ax
+	mov dx, ax
 	
+	mov ax, 0x0020
+	mul WORD [bpbNumberOfRootEnt]
+	div WORD [bpbBytesPerSector]
+	xchg ax, cx                     ;cx now has number of sectors used by root directory
 	
-	cli
-	hlt
+	mov al, BYTE [bpbNumberOfFATs]
+	mul WORD [bpbSectorsPerFAT]
+	add ax, WORD [bpbReservedSectors]	;ax now has number of sectors before root directory by 0-based addressing
+	
+	mov bx, 0x0200
+	call _GetSectors
 
 __LOAD_FAT:
 
 __LOAD_SECOND_STAGE_SECTS:
+
+__END_AND_FAILURE:
+	cli
+	hlt
 
 ;#########################################
 ;############### Zero out remaining bytes
