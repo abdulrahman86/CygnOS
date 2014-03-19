fd88939b2c468b3d1f6822c2085c65596c879239
Sunny Shah <shahsunny712@gmail.com>
2014-03-19 23:25:06 +0530

    Change label, function and variable names

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,7 +1,7 @@
 bits 16
-org 0
+org 0x0
 
-jmp __SEGMENTS_INIT
+jmp __SEGMENTS_INIT__
 
 bpbOEMName		DB "Sunny S "
 ;##########################################
@@ -31,50 +31,51 @@ ebpbFileSystem		 :	DB "FAT12   "
 ;############ Bootloader code begins here
 ;#########################################
 
-__VARIABLES_DECL:
+__VARIABLES_DECL__:
 	msgBootStarting  DB 0x0D, 0x0A, "**************************", 0x0D, 0x0A, "Boot starting...", 0x0D, 0x0A, 0x00
 	msgFailure 		 DB "Error in loading the operating system...", 0x0D, 0x0A, 0x00
 	msgBootComplete	 DB "Boot finished.", 0x0D, 0x0A, 0x00
-	secondStageName  DB "INITKRNLBIN"
-	nextCluster  	 DW 0
-	bufStart	  	 DW 0x0200
-	dataRegion	  	 DW 0
+	nameSecondStage  DB "INITKRNLBIN"
+	numNextCluster 	 DW 0
+	addrBufStart	 DW 0x0200
+	addrDataRegion 	 DW 0
 	
-__FUNCTIONS_DECL:
-	_PrintMessage:
+__FUNCTIONS_DECL__:
+
+	__PrintMessage_:
 		lodsb
 		or al, al
-		jz Done
+		jz _Done_
 		mov ah, 0Eh
 		int 10h
-		jmp _PrintMessage
-	Done:
+		jmp __PrintMessage_
+	_Done_:
 		ret
 	
-	_GetSectors:
+	__GetSectors_:
 		push ax
 		push bx
 		push cx
 		
-		call _LBAtoCHS
+		call __LBAtoCHS_
 		mov ah, 0x02
 		mov al, 0x01
 		mov dl, BYTE [ebpbDriveNumber]
 		int 13h
-		jnc ReadSuccess
+		jnc _ReadSuccess_
 		
-		jmp __END_AND_FAILURE
+		jmp __END_AND_FAILURE__
 		
-	ReadSuccess:
+	_ReadSuccess_:
 		pop cx
 		pop bx
 		pop ax
 		inc ax								;Point to next sector to read from 
 		add bx, WORD [bpbBytesPerSector]	;advance write pointer to next block
-		loop _GetSectors
+		loop __GetSectors_
 		ret
 	
-	_LBAtoCHS:
+	__LBAtoCHS_:
 		xor dx, dx
 		div WORD [bpbSectorsPerTrack]
 		inc dl
@@ -85,15 +86,15 @@ __FUNCTIONS_DECL:
 		mov ch, al
 		ret
 		
-	_ClusterToLBA:
+	__ClusterToLBA_:
 		sub ax, 0x0002
 		xor cx, cx
 		mov cl, BYTE [bpbSectorsPerCluster]
 		mul cl
-		add ax, WORD [dataRegion]
+		add ax, WORD [addrDataRegion]
 		ret
 
-__SEGMENTS_INIT:
+__SEGMENTS_INIT__:
 	cli
 	
 	mov ax, 0x07C0
@@ -108,9 +109,9 @@ __SEGMENTS_INIT:
 	
 	sti
 	
-__LOAD_ROOT:
+__LOAD_ROOT__:
 	mov si, msgBootStarting
-	call _PrintMessage
+	call __PrintMessage_
 	
 	xor ax, ax
 	mov cx, ax
@@ -125,58 +126,58 @@ __LOAD_ROOT:
 	mul WORD [bpbSectorsPerFAT]
 	add ax, WORD [bpbReservedSectors]	;ax now has number of sectors before root directory by 0-based addressing
 	
-	mov WORD [dataRegion], ax
-	add WORD [dataRegion], cx
+	mov WORD [addrDataRegion], ax
+	add WORD [addrDataRegion], cx
 	
-	mov bx, WORD [bufStart]
-	call _GetSectors
+	mov bx, WORD [addrBufStart]
+	call __GetSectors_
 	
 	mov cx, WORD [bpbNumberOfRootEnt]
-	mov di, WORD [bufStart]
+	mov di, WORD [addrBufStart]
 	
-   _ROOT_FIND_II_STAGE:
-   	mov si, secondStageName
+   _ROOT_FIND_II_STAGE_:
+   	mov si, nameSecondStage
    	push cx
    	push di
    	mov cx, 0x000B
    	repe cmpsb
    	cmp cx, 0x0000
    	pop di   	
-   	jz _FIND_FIRST_CLUSTER_II_STAGE   	
+   	jz _FIND_FIRST_CLUSTER_II_STAGE_  	
    	add di, 0x0020
    	pop cx   	
-   	loop _ROOT_FIND_II_STAGE
+   	loop _ROOT_FIND_II_STAGE_
    	
-	jmp __END_AND_FAILURE
+	jmp __END_AND_FAILURE__
 	
-   _FIND_FIRST_CLUSTER_II_STAGE:
+   _FIND_FIRST_CLUSTER_II_STAGE_:
    	mov cx, WORD [di + 0x001A]
-   	mov WORD [nextCluster], cx
+   	mov WORD [numNextCluster], cx
    	
-__LOAD_FAT:
+__LOAD_FAT__:
 	mov cx, WORD [bpbSectorsPerFAT]
 	mov ax, WORD [bpbReservedSectors]
 	
-	mov bx, WORD [bufStart]
-	call _GetSectors
+	mov bx, WORD [addrBufStart]
+	call __GetSectors_
 	
 	mov ax, 0x0050						;Location where second stage will be loaded
 	mov es, ax
 	mov bx, 0x0000						;Location where second stage will be loaded
 	push bx
 	
-__LOAD_SECOND_STAGE_SECTS:
-	mov ax, WORD [nextCluster]	
-	call _ClusterToLBA
+__LOAD_SECOND_STAGE_SECTS__:
+	mov ax, WORD [numNextCluster]	
+	call __ClusterToLBA_
 	
 	xor cx, cx
 	mov cl, BYTE [bpbSectorsPerCluster]
 	pop bx
-	call _GetSectors					;Get file cluster
+	call __GetSectors_					;Get file cluster
 	push bx
 
-   _READ_ALL_SECTS:
-   	mov ax, WORD [nextCluster]
+   _READ_ALL_SECTS_:
+   	mov ax, WORD [numNextCluster]
 	mov cx, ax
 	mov dx, ax
 	shr cx, 0x0001						;Divide cluster number by two
@@ -185,29 +186,29 @@ __LOAD_SECOND_STAGE_SECTS:
 	add bx, dx
 	mov dx, WORD [bx]
 	test ax, 0x0001						;Similar to AND but does not change register values
-	jz _EVEN_CLUSTER
+	jz _EVEN_CLUSTER_
 	
 	shr dx, 0x0004
-	jmp _CHECK_LAST_CLUSTER
+	jmp _CHECK_LAST_CLUSTER_
 	
-   _EVEN_CLUSTER:
+   _EVEN_CLUSTER_:
     and dx, 0x0FFF
     
-   _CHECK_LAST_CLUSTER:
+   _CHECK_LAST_CLUSTER_:
     cmp dx, 0x0FF0
-    mov WORD [nextCluster], dx			;dx has next cluster number
-    jnz __LOAD_SECOND_STAGE_SECTS
+    mov WORD [numNextCluster], dx			;dx has next cluster number
+    jnz __LOAD_SECOND_STAGE_SECTS__
     
     mov si, msgBootComplete
-    call _PrintMessage
+    call __PrintMessage_
     
     push WORD 0x0050					;CS for second stage
     push WORD 0x0000					;IP for second stage
     retf
 	
-__END_AND_FAILURE:
+__END_AND_FAILURE__:
 	mov si, msgFailure
-	call _PrintMessage
+	call __PrintMessage_
 	mov ah, 0x0
 	int 16h								;Wait for keypress (ah=0)
 	int 19h								;Reboot
