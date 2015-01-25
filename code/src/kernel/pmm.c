2189fb0f839d53a3e41e0d5f651e3cf683139169
Sunny Shah <shahsunny712@gmail.com>
2015-01-25 23:32:51 +0530

    Change logic for preventing kernel pages from being considered free: Resize the memory region to account for kernel pages before initializing it.

--- code/src/kernel/pmm.c
+++ code/src/kernel/pmm.c
@@ -15,16 +15,36 @@ uint64_t next_page = 0;
 static int prev_page_initialized = 0;
 
 extern uint32_t _START, _END;
+static uint64_t kstart_addr, kend_addr;
 
 void pmm_init(uint64_t __memory_total)
 {
 	memory_total = __memory_total;
 	pages_total = memory_total/PAGE_SIZE;
+
+	kstart_addr = *(uint64_t *)&_START;
+	kend_addr = *(uint64_t *)&_END - 1;
 }
 
 void pmm_region_init(uint64_t __base, uint64_t __length)
 {
 	uint64_t page_index;
+	uint64_t region_end_addr = __base + __length - 1;
+
+	if(__base < kstart_addr)
+	{
+		if(region_end_addr >= kstart_addr && region_end_addr <= kend_addr)
+			__length -= (region_end_addr - __base + 1);
+		//if region_end_addr > kend_addr, need to split the region into two parts
+	}
+	else if(__base <= kend_addr)
+	{
+		if(region_end_addr <= kend_addr)
+			return;
+		
+		__length -= (kend_addr - __base + 1);
+		__base = kend_addr + 1;
+	}
 
 	if(__base%PAGE_SIZE != 0)
 		__base = (__base/PAGE_SIZE)*PAGE_SIZE + PAGE_SIZE;
@@ -34,15 +54,12 @@ void pmm_region_init(uint64_t __base, uint64_t __length)
 		
 	for(page_index = __base; page_index < (__base + __length); page_index += PAGE_SIZE)
 	{
-		if(page_index < *(uint64_t *)&_START || page_index >= *(uint64_t *)&_END)
-		{
-			if(prev_page_initialized)
-				*(uint64_t *)prev_page = page_index;
+		if(prev_page_initialized)
+			*(uint64_t *)prev_page = page_index;
 				
-			prev_page = page_index;
-			if(!prev_page_initialized)
-				prev_page_initialized = 1;
-		}
+		prev_page = page_index;
+		if(!prev_page_initialized)
+			prev_page_initialized = 1;
 	}
 }
 		
