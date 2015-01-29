ad0812e50edb22eff78d75ac25c6f32bc37d1fe8
Sunny Shah <shahsunny712@gmail.com>
2015-01-29 22:14:05 +0530

    Using the _START and _END symbols from linker script in the correct manner. These addresses are now used without offsetting them to determine region/kernel overlap.
    Changed pointer types from uint64_t to uint32_t, since this is an x86 kernel.

--- code/src/include/pmm.h
+++ code/src/include/pmm.h
@@ -6,13 +6,13 @@
 #define	PAGE_SIZE	4096
 
 //holds the address of the next free page in physical memory
-extern uint64_t next_page;
+extern uintptr_t next_page;
 
 //initialize the pmm by setting the total physical memory in KB and the total number of pages
 extern void pmm_init(uint64_t __memory_total);
 
 //adds the pages of the block of memory marked as available by the BIOS to the list of free pages.
 //__base is the base address of the region, and __length is its length
-extern void pmm_init_region(uint64_t __base, uint64_t __length);
+extern void pmm_region_init(uint32_t __base, uint32_t __length);
 
 #endif
--- code/src/kernel/pmm.c
+++ code/src/kernel/pmm.c
@@ -1,4 +1,5 @@
 #include <pmm.h>
+#include <string.h>
 #include <screen_vga.h>
 
 //represents the total number of pages available in physical memory
@@ -8,42 +9,44 @@ static uint64_t pages_total = 0;
 static uint64_t memory_total = 0;
 
 //holds the address of the previous page that was marked free, so that the address of the next page can be written to it when iterating over the pages
-static uint64_t prev_page = 0;
+static uint32_t prev_page = 0;
 
-uint64_t next_page = 0;
+uint32_t next_page = 0;
 
 static int prev_page_initialized = 0;
 
 extern uint32_t _START, _END;
-static uint64_t kstart_addr, kend_addr;
+static uint32_t kstart_addr, kend_addr;
 
 void pmm_init(uint64_t __memory_total)
 {
 	memory_total = __memory_total;
 	pages_total = memory_total/PAGE_SIZE;
 
-	kstart_addr = *(uint64_t *)&_START;
-	kend_addr = *(uint64_t *)&_END - 1;
+	kstart_addr = (uint32_t)&_START;
+	kend_addr = (uint32_t)&_END;
 }
 
-void pmm_region_init(uint64_t __base, uint64_t __length)
+void pmm_region_init(uint32_t __base, uint32_t __length)
 {
-	uint64_t page_index;
-	uint64_t region_end_addr = __base + __length - 1;
-
+	uint32_t page_index;
+	uint32_t region_end_addr = __base + __length;
+	int kernel_in_region = 0;
+	
 	if(__base < kstart_addr)
 	{
-		if(region_end_addr >= kstart_addr && region_end_addr <= kend_addr)
-			__length -= (region_end_addr - __base + 1);
-		//if region_end_addr > kend_addr, need to split the region into two parts
+		if(region_end_addr > kstart_addr && region_end_addr <= kend_addr)
+			__length -= (region_end_addr - __base);
+		else if(region_end_addr > kend_addr)
+			kernel_in_region = 1;
 	}
-	else if(__base <= kend_addr)
+	else if(__base < kend_addr)
 	{
 		if(region_end_addr <= kend_addr)
 			return;
 		
 		__length -= (kend_addr - __base + 1);
-		__base = kend_addr + 1;
+		__base = kend_addr;
 	}
 
 	if(__base%PAGE_SIZE != 0)
@@ -54,8 +57,11 @@ void pmm_region_init(uint64_t __base, uint64_t __length)
 		
 	for(page_index = __base; page_index < (__base + __length); page_index += PAGE_SIZE)
 	{
+		if(kernel_in_region && page_index >= kstart_addr && page_index < kend_addr)
+			continue;
+		
 		if(prev_page_initialized)
-			*(uint64_t *)prev_page = page_index;
+			*(uint32_t *)prev_page = page_index;
 				
 		prev_page = page_index;
 		if(!prev_page_initialized)
